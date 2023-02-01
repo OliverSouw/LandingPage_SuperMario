40e91cc10a55e6a4f4c7cf38ac90cfc2fbb3b1c1
Igor Oliveira de Sousa <igor.oliveira@rfsolucoes.local>
2023-02-01 15:45:29 -0300

    corrigindo video

--- src/js/script.js
+++ src/js/script.js
@@ -1,12 +1,12 @@
 let btnTrailer = document.querySelector('.botao-trailer');
 let modal = document.querySelector('.modal');
 let btnCloseModal = document.querySelector('.fechar-modal');
-let video = document.querySelector('#video');
+let video = document.querySelector('iframe');
 
 btnTrailer.addEventListener('click', () => {
     modal.style.visibility = 'visible';
-})
+});
 
 btnCloseModal.addEventListener('click', () => {
     modal.style.visibility = 'hidden';
-})
+});
